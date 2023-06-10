bcad0f56a78be46b522cc77075232255e1da1286
WonYus <lucy_oliv@outlook.com>
2023-06-11 03:06:00 +0700

    update host

--- bot/cogs/music.py
+++ bot/cogs/music.py
@@ -334,9 +334,9 @@ class Music(commands.Cog, wavelink.WavelinkMixin):
 
         nodes = {
             "MAIN": {
-                "host": "127.0.0.1",
+                "host": "0.0.0.0",
                 "port": 2333,
-                "rest_uri": "http://127.0.0.1:2333",
+                "rest_uri": "http://0.0.0.0:2333",
                 "password": "youshallnotpass",
                 "identifier": "MAIN",
                 "region": "europe",
